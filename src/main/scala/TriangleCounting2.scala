eb5e6040a195bb3e49d4b59d5c5b94dcdfd69850
Nick Lupashko <n.r.lupashko@gmail.com>
2017-07-17 18:24:45 +0300

    [TriangleCounting2] Make the code more understandable

--- src/main/scala/TriangleCounting2.scala
+++ src/main/scala/TriangleCounting2.scala
@@ -3,17 +3,17 @@ import org.apache.spark.sql.SparkSession
 object TriangleCounting2 {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
-      .appName("TriangleCounting")
+      .appName("TriangleCounting2")
       .master("local[*]")
       .getOrCreate
     val sc = spark.sparkContext
 
-    val graph = sc.textFile("graph.txt").flatMap(s => {
-      val a = s.split(" ").map(_.toInt).sorted
-      if (a(0) == a(1)) List()
-      else List((a(0), a(1)))
+    val graph = sc.textFile("graph.txt").flatMap(edge => {
+      val vertices = edge.split(" ").map(_.toInt).sorted
+      if (vertices(0) == vertices(1)) List()
+      else List((vertices(0), vertices(1)))
     }).distinct.cache
-	
+
     println(graph.join(graph).keyBy(_._2).join(graph.map(_ -> 1)).count)
 
     spark.stop
