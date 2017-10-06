89718b17d96e14efc004bf093d37049ad548993f
Nick Lupashko <n.r.lupashko@gmail.com>
2017-10-06 15:15:44 +0300

    [InvertedIndex] Improve readability

--- src/main/scala/InvertedIndex.scala
+++ src/main/scala/InvertedIndex.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.sql.SparkSession
 
+import scala.io.Source
+
 object InvertedIndex {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
@@ -12,29 +14,31 @@ object InvertedIndex {
       * Created for `20 Newsgroup DataSet` from
       * http://www.cs.cmu.edu/afs/cs/project/theo-20/www/data/news20.html
       */
-    val badStart = Set("xref:", "path:", "from:", "newsgroups:",
-      "subject:", "summary:", "keywords:", "message-id:", "date:",
-      "expires:", "followup:", "distribution:", "organization:",
-      "approved:", "supersedes:", "lines:", "archive-name:", "to:",
-      "last-modified:", "version:", "article-i.d.:", "subj:", "cc:",
-      "references:", "nntp-posting-host:", "received:", "reply-to:",
-      "content-length:", "followup-to:", "sender:")
+    val ignore = Source.fromFile("ignore.txt").getLines().toSet
     val regex = "[a-z]+('[a-z]+)?".r
+    val path = "path/to/20_newsgroup/*"
 
-    sc.wholeTextFiles("path/to/20_newsgroup/*")
+    sc.wholeTextFiles(path)
       .map {
         case (path, text) => path.replaceFirst(".+/(.+/.+)", "$1") ->
           text.toLowerCase.split("\n")
-            .filter(line => !badStart.exists(line.startsWith(_)))
+            .filter(line => !ignore.exists(line.startsWith(_)))
             .flatMap(regex.findAllIn(_).toList)
       }
       .flatMap {
         case (path, words) => words.map(word => ((word, path), 1))
       }
       .reduceByKey(_ + _)
-      .map { case ((word, path), count) => (word, (count, path)) }
-      .reduceByKey((a, b) => (a._1 + b._1, s"${a._2} ${b._2}"))
-      .map { case (word, (count, paths)) => s"$word, $count, $paths" }
+      .map {
+        case ((word, path), count) => (word, (count, path))
+      }
+      .reduceByKey {
+        case ((leftCount, leftPath), (rightCount, rightPath)) =>
+          (leftCount + rightCount, s"$leftPath $rightPath")
+      }
+      .map {
+        case (word, (count, paths)) => s"$word, $count, $paths"
+      }
       .saveAsTextFile("index")
 
     spark.stop
